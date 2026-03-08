0b9961b1eeb7883e12dcc3024ac0e769c8b5c6c3
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-03-08 17:00:19 +0000

    Added parameterised rendering

--- player.gd
+++ player.gd
@@ -7,9 +7,9 @@ func _process(delta: float) -> void:
 	var horizontal_vector := Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards");
 	var vertical_magnitude = Input.get_action_strength("move_up") - Input.get_action_strength("move_down");
 	
-	position += transform.basis.get_rotation_quaternion() * Vector3(horizontal_vector.x, vertical_magnitude, horizontal_vector.y) * delta * 10;
+	position += (transform.basis.get_rotation_quaternion() * Vector3(horizontal_vector.x, 0, horizontal_vector.y) + Vector3(0, vertical_magnitude, 0)) * delta * 10;
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotation.y += -event.relative.x / 200
-		rotation.x += -event.relative.y / 200
+		rotation.y += -event.relative.x / 200;
+		rotation.x = clamp(rotation.x - event.relative.y / 200, -PI/2, PI/2);
--- twisting_vine_test.gd
+++ twisting_vine_test.gd
@@ -4,45 +4,65 @@ const unscaled_a := 1.00005507808
 const unscaled_b := 0.55342925736
 const unscaled_c := 0.998737689 - unscaled_a
 
-@export var radius: float
-
-@export var length: float
-
-@export var rotation_angles: Vector3
-
+@export var radius: float:
+	set(value):
+		radius = value;
+		if (is_node_ready()):
+			update_curve();
+			update_mesh();
+@export var length: float:
+	set(value):
+		length = value;
+		if (is_node_ready()):
+			update_curve();
+			update_mesh();
 @export var mesh: Mesh
+@export var quarter_turns: int:
+	set(value):
+		quarter_turns = value;
+		if (is_node_ready()):
+			update_curve();
+			update_mesh();
 
-var instance_cache: Array[RID]
+var instance_cache: Array[RID];
 
 func _ready() -> void:
+	curve.bake_interval = 0.05
+	
+	update_curve()
+	update_mesh()
+
+func update_curve() -> void:
+	curve.clear_points();
+
 	var a := unscaled_a * radius;
 	var b := unscaled_b * radius;
 	var c := unscaled_c * radius;
-	
-	var lenGrad := sqrt(c*c+b*b) * length/8
 
-	var pointA := position + Vector3(a, -length/2, 0);
+	var lenGrad := length/quarter_turns/PI
+
+	var pointA := position + Vector3(a, 0, 0);
 	var pointAIn := Vector3(c, -lenGrad, -b)
 	var pointAOut := Vector3(c, lenGrad, b)
-	var pointB := position + Vector3(0, -length/4, a);
+	var pointB := position + Vector3(0, 0, a);
 	var pointBIn := Vector3(b, -lenGrad, c)
 	var pointBOut := Vector3(-b, lenGrad, c)
 	var pointC := position + Vector3(-a, 0, 0);
 	var pointCIn := Vector3(-c, -lenGrad, b)
 	var pointCOut := Vector3(-c, lenGrad, -b)
-	var pointD := position + Vector3(0, length/4, -a);
+	var pointD := position + Vector3(0, 0, -a);
 	var pointDIn := Vector3(-b, -lenGrad, -c)
 	var pointDOut := Vector3(b, lenGrad, -c)
-
-	curve.bake_interval = 0.05
 	
-	curve.add_point(pointA, Vector3.ZERO, pointAOut);
-	curve.add_point(pointB, pointBIn, pointBOut);
-	curve.add_point(pointC, pointCIn, pointCOut);
-	curve.add_point(pointD, pointDIn, pointDOut);
-	curve.add_point(pointA + Vector3(0, length, 0), pointAIn, Vector3.ZERO);
-
-	update_mesh()
+	for i in quarter_turns+1:
+		if i % 4 == 0:
+			curve.add_point(pointA + Vector3(0, length/quarter_turns*(i-quarter_turns/2.0), 0), pointAIn, pointAOut);
+		elif i % 4 == 1:
+			curve.add_point(pointB + Vector3(0, length/quarter_turns*(i-quarter_turns/2.0), 0), pointBIn, pointBOut);
+		elif i % 4 == 2:
+			curve.add_point(pointC + Vector3(0, length/quarter_turns*(i-quarter_turns/2.0), 0), pointCIn, pointCOut);
+		else:
+			curve.add_point(pointD + Vector3(0, length/quarter_turns*(i-quarter_turns/2.0), 0), pointDIn, pointDOut);
 
 func update_mesh() -> void:
 	var baked_points = curve.get_baked_points();
@@ -60,3 +80,8 @@ func update_mesh() -> void:
 			instance_cache.append(render_instance);
 		else:
 			RenderingServer.instance_set_transform(instance_cache[point_id], xform);
+			RenderingServer.instance_set_visible(instance_cache[point_id], true);
+	
+	if (len(instance_cache) > len(baked_points)):
+		for i in range(len(instance_cache)-1, len(baked_points)-1, -1):
+			RenderingServer.instance_set_visible(instance_cache[i], false);
