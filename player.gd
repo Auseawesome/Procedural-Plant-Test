ebc800e01111ff0f2f9aa8589bf7682ea3dcf887
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-03-11 19:54:43 +0000

    Slowed player movement

--- player.gd
+++ player.gd
@@ -7,7 +7,7 @@ func _process(delta: float) -> void:
 	var horizontal_vector := Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards");
 	var vertical_magnitude = Input.get_action_strength("move_up") - Input.get_action_strength("move_down");
 	
-	position += (transform.basis.get_rotation_quaternion() * Vector3(horizontal_vector.x, 0, horizontal_vector.y) + Vector3(0, vertical_magnitude, 0)) * delta * 10;
+	position += (transform.basis.get_rotation_quaternion() * Vector3(horizontal_vector.x, 0, horizontal_vector.y) + Vector3(0, vertical_magnitude, 0)) * delta * 2;
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
